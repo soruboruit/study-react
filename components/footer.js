c3ea9a2f6a88b912b83bdce4e4385831081e9764
Soru <ityouna007@gmail.com>
2023-09-08 02:49:40 +0900

    fix footer and add import image from footer

--- components/footer.js
+++ components/footer.js
@@ -1,21 +1,24 @@
 import styles from "../styles/Home.module.css";
+import Image from "next/image"; // Image コンポーネントをインポート
 
 export function Footer() {
-    return(
-        <footer className={styles.footer}>
-            <div className='footer'>
-        <div className='footer-inner'>
-          <div className='footer-logo'>
-            <img src='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/type_logo.png' />
-            <p>Learn to code, learn to creative.</p>
-          </div>
-          <ul className='footer-list'>
-            <li>会社概要</li>
-            <li>採用</li>
-            <li>お問い合わせ</li>
-          </ul>
+    return (
+        <div>
+            <a
+                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Thanks By{' '}
+                <Image
+                    src="/vercel.svg"
+                    alt="Vercel Logo"
+                    className={styles.vercelLogo}
+                    width={100}
+                    height={24}
+                    priority
+                />
+            </a>
         </div>
-      </div>
-        </footer>
-    )
+    );
 }
