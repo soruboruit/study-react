a345073c053d04d6256d48d6d9e2df54309cc68e
Soru <ityouna007@gmail.com>
2023-09-14 23:13:09 +0900

    ✨ add main compponents

---
+++ components/main.js
@@ -0,0 +1,19 @@
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import { Links }from 'components/links'
+import { Headline } from 'components/headline'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export  function Main(props) {
+  return (
+      <main className={`${styles.main} ${inter.className}`}>
+        <Headline page={props.page}
+        >
+           <code className={styles.code}>pages/{props.page}.js</code>
+        </Headline>
+        <div className={styles.center}>{props.page}</div>
+      <Links></Links>
+      </main>
+  );
+}
