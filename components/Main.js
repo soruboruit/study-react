19441947bb4998340454dfb599737bcb08a78855
Soru <ityouna007@gmail.com>
2023-09-14 21:14:58 +0900

    ✨ add headline components

---
+++ components/Main.js
@@ -0,0 +1,7 @@
+<main className={`${styles.main} ${inter.className}`}>
+        <Headline page="Index Page!!">
+          <code className={styles.code}>pages/index.js</code>
+        </Headline>
+        <div className={styles.center}>say oorchlson index</div>
+      <Links></Links>
+      </main>
