0a954ce22d714e134a45aab72388f68525943cf9
Soru <ityouna007@gmail.com>
2023-09-14 21:09:57 +0900

    ✨  🔥 要らないコードを消した。childrenを加えた

--- components/headline.js
+++ components/headline.js
@@ -1,14 +1,12 @@
 import styles from "../styles/Home.module.css"
 
 export  function Headline(props) {
-  console.log(props);
     return (
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            {props.code}
+            {props.children}
           </p>
-          <button onClick={props.onClick}>ボタン</button>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import { Footer} from '/components/footer'
-import { Links }from 'components/links'
-import { Headline } from 'components/headline'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
+import { Footer} from '/components/Footer';
+import { Links }from '/components/Links';
+import { Headline } from '/components/Headline';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,12 +18,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <Headline page="Index Page!!" number ={1111} array={[1,2,3]}
-        obj={{foo: "foo", bar: "bar"}}
-        boolean={true}
-        code={<div>  <code className={styles.code}>pages/index.js</code> </div>}
-        onClick={() => alert("クリック")}
-        />
+        <Headline page="Index Page!!">
+          <code className={styles.code}>pages/index.js</code>
+        </Headline>
         <div className={styles.center}>say oorchlson index</div>
       <Links></Links>
       </main>
