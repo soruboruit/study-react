8fc77e3f9efb00760895f176d552dfe51cf563cb
Soru <ityouna007@gmail.com>
2023-09-21 01:39:22 +0900

    ♻️ remove code

--- components/header.js
+++ components/header.js
@@ -1,23 +1,11 @@
 import classes from "./footer.module.css";
 import Image from "next/image"; // Image コンポーネントをインポート
 
-export function Footer() {
+export function Header() {
     return (
-       <footer className={classes.header}>
-            <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                
-            >
-                Thanks By{' '}
-                <Image
-                    src="/vercel.svg"
-                    alt="Vercel Logo"
-                    className={classes.vercelLogo}
-                    width={100}
-                    height={24}
-                    
-                />
-            </a>
+       <footer >
+
+        
         </footer>
     );
 }
