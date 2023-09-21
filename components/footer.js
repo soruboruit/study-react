8925dbdf8cd83ea3034e7344e023713700909429
Soru <ityouna007@gmail.com>
2023-09-21 23:32:16 +0900

    ♻️ fix code

--- components/footer.js
+++ components/footer.js
@@ -2,23 +2,22 @@ import classes from "./footer.module.css";
 import Image from "next/image"; // Image コンポーネントをインポート
 
 export function Footer() {
-    return (
-       <footer className={classes.footer}>
-            <a
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Thanks By{' '}
-                <Image
-                    src="/vercel.svg"
-                    alt="Vercel Logo"
-                    className={classes.vercelLogo}
-                    width={100}
-                    height={24}
-                    
-                />
-            </a>
-        </footer>
-    );
+  return (
+    <footer className={classes.footer}>
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Thanks By{" "}
+        <Image
+          src="/vercel.svg"
+          alt="Vercel Logo"
+          className={classes.vercelLogo}
+          width={100}
+          height={24}
+        />
+      </a>
+    </footer>
+  );
 }
