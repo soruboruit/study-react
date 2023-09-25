0b3e89c21552a65993e577e89b93fec2d71a3950
Soru <ityouna007@gmail.com>
2023-09-26 07:02:43 +0900

    ♻️ remove inter fixed structure

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,19 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import { Footer} from '/components/footer'
-import { Main } from 'components/main'
-import { Header } from 'components/header'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head';
+import { Footer } from '/components/footer';
+import { Main } from 'components/main';
+import { Header } from 'components/header';
+import styles from '../styles/Home.module.css'; // 追加
 
 export default function Home() {
   return (
-    <div className={Home.container}>
+    <div className={styles.container}>
       <Head>
         <title>Index Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
-      <Main page="index"></Main>
-      
+      <Header />
+      <Main page="index" />
       <Footer />
     </div>
-  )
+  );
 }
