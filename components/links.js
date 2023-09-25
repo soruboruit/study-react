646008ba9956f25faa96230f14c3cbd070f5de24
Soru <ityouna007@gmail.com>
2023-09-26 06:58:22 +0900

    use key also fixed other

--- components/links.js
+++ components/links.js
@@ -3,25 +3,25 @@ import classes from "./links.module.css";
 const ITEMS = [
   {
     href: "https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: " Docs <span>-&gt;</span>",
+    title: " Docs →",
     description:
       "Find in-depth information about Next.js features and&nbsp;API.",
   },
   {
     href: "https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: " Learn <span>-&gt;</span>",
+    title: " Learn →",
     description:
       " Learn about Next.js in an interactive course with&nbsp;quizzes!",
   },
   {
     href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: " Deploy <span>-&gt;</span>",
+    title: " Examples →",
     description:
       " Discover and deploy boilerplate example Next.js&nbsp;projects.",
   },
   {
     href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
-    title: "  Deploy <span>-&gt;</span>",
+    title: "  Deploy →",
     description:
       "Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.",
   },
@@ -30,13 +30,12 @@ const ITEMS = [
 export function Links() {
   return (
     <div className={classes.grid}>
-      {ITEMS.map((item) => {
+      {ITEMS.map((item, index) => {
         return (
-          <a key={item.href} href={item.href} className={classes.card}>
-            <h2 className="{classes.title}">{item.title}</h2>
+          <a key={index} href={item.href} className={classes.card}>
+            <h2 className={classes.title} dangerouslySetInnerHTML={{ __html: item.title }}></h2>
             <p className={classes.description}>{item.description}</p>
           </a>
-        
         );
       })}
     </div>
