98e0ef6447f04e85f7ad0c61d8c6b41a77b6a5a1
Soru <ityouna007@gmail.com>
2023-09-26 06:59:45 +0900

    ♻️ remove inter also fixed other

--- components/main.js
+++ components/main.js
@@ -1,17 +1,14 @@
-import { Inter } from 'next/font/google'
-import classes from './main.module.css'
-import { Links }from 'components/links'
-import { Headline } from 'components/headline'
+import classes from './main.module.css';
+import { Links } from 'components/links';
+import { Headline } from 'components/headline';
 
-const inter = Inter({ subsets: ['latin'] })
-
-export  function Main(props) {
+export function Main(props) {
   return (
-      <main className={`${classes.main} ${inter.className}`}>
-        <Headline page={props.page} >
-          <code className={classes.code}>Pages/{props.page}.js</code>
-        </Headline>
-      <Links/>
-      </main>
-  )
+    <main className={classes.main}>
+      <Headline page={props.page}>
+        <code className={classes.code}>Pages/{props.page}.js</code>
+      </Headline>
+      <Links />
+    </main>
+  );
 }
