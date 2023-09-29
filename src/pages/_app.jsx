2979184e597723a805aedb68cec12d31dc5bd0c1
Soru <ityouna007@gmail.com>
2023-09-29 15:05:56 +0900

    ♻️ 固定したiconを追加した

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,12 @@
-import '../styles/globals.css'; // 相対パスを使用
+import "../styles/globals.css"; // 相対パスを使用
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
